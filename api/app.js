603b172c025f7418e149adaee02ff241437c6b99
Marcus Ícaro <marcusicaromc@gmail.com>
2023-11-11 16:36:00 -0300

    check conflicts

--- api/app.js
+++ api/app.js
@@ -90,7 +90,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.json(err.message);
+  res.json({error: err.message});
 });
 
 app.listen(3002, () => console.log('app listening on port 3002!'));
