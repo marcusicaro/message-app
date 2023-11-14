11f57eeb9b45fd32ec7653e8d4f0de32061bc481
Marcus <marcusicaromc@gmail.com>
2023-11-14 07:19:02 -0300

    wip home login

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -5,40 +5,97 @@ export default function Home() {
   return (
     <div className='flex w-screen h-screen'>
       <div className='w-42 h-full flex items-center justify-center left-0 absolute rounded-md bg-white z-10'>
-        <div className='relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 '>
-          <div className='relative bg-white dark:bg-gray-900 rounded-md flex flex-col gap-4'>
-            <div>
-              <label
-                htmlFor='login'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-              >
-                Login
-              </label>
-              <input
-                type='text'
-                id='login'
-                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Login'
-                required
-              />
-            </div>
-            <div className='mb-6'>
-              <label
-                htmlFor='password'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-              >
-                Password
-              </label>
-              <input
-                type='password'
-                id='password'
-                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Password'
-                required
-              />
+        <section className='bg-gray-50 dark:bg-gray-900'>
+          <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
+            <a
+              href='#'
+              className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
+            ></a>
+            <div className='relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600  dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800'>
+              <div className='p-6 bg-white space-y-4 md:space-y-6 sm:p-8 rounded-lg'>
+                <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+                  Sign in to your account
+                </h1>
+                <form className='space-y-4 md:space-y-6' action='#'>
+                  <div>
+                    <label
+                      htmlFor='email'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type='email'
+                      name='email'
+                      id='email'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      placeholder='name@company.com'
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor='password'
+                      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                    >
+                      Password
+                    </label>
+                    <input
+                      type='password'
+                      name='password'
+                      id='password'
+                      placeholder='••••••••'
+                      className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      required
+                    />
+                  </div>
+                  <div className='flex items-center justify-between'>
+                    <div className='flex items-start'>
+                      <div className='flex items-center h-5'>
+                        <input
+                          id='remember'
+                          aria-describedby='remember'
+                          type='checkbox'
+                          className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800'
+                          required
+                        />
+                      </div>
+                      <div className='ml-3 text-sm'>
+                        <label
+                          htmlFor='remember'
+                          className='text-gray-500 dark:text-gray-300'
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                    </div>
+                    <a
+                      href='#'
+                      className='text-sm font-medium text-primary-600 hover:underline dark:text-primary-500'
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                  <button
+                    type='submit'
+                    className='w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
+                  >
+                    Sign in
+                  </button>
+                  <p className='text-sm font-light text-gray-500 dark:text-gray-400'>
+                    Don't have an account yet?{' '}
+                    <a
+                      href='#'
+                      className='font-medium text-primary-600 hover:underline dark:text-primary-500'
+                    >
+                      Sign up
+                    </a>
+                  </p>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
       <div className='w-7/12 h-full right-0 absolute'>
         <Image alt='Chatime background' src={chatimePicture} fill={true} />
--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#569DAA',
+        secondary: '#ecc94b',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,5 +20,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
