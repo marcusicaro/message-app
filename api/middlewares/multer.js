da461ee22d31e661f7dc5895d29f2a37da8f0b8b
marcusicaro <marcusicaromc@gmail.com>
2024-02-12 09:23:20 -0300

    multer saving files with extension

--- api/middlewares/multer.js
+++ api/middlewares/multer.js
@@ -1,17 +1,26 @@
-const multer  = require('multer')
+const multer = require('multer');
 const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
 const upload = multer({
-    dest: './uploads', //multer settings
+    storage: storage,
     fileFilter: function (req, file, callback) {
         var ext = path.extname(file.originalname);
         if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-            return callback(new Error('Only images are allowed'))
+            return callback(new Error('Only images are allowed'));
         }
-        callback(null, true)
+        callback(null, true);
     },
     limits:{
-        fileSize: 1024 * 1024
+        fileSize:  1024 *  1024
     }
 }).single('avatar');
 
